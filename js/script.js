78e3aae1665b19a0c51d79d46aca596ea4f8d170
Dragos <dragosedelea@mai.ru>
2022-03-07 11:21:36 +0200

    Optimisation, Completion with correct information..

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function show() {
+    var formular = document.getElementById("form");
+    if (formular.style.display === "") {
+        formular.style.display = "block";
+    } else if (formular.style.display === "none") {
+        formular.style.display = "block";
+    } else {
+        formular.style.display = "none";
+
+    }
+}
