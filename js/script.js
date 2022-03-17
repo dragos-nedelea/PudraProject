868cefefb3e5ee77237e11747ce3f4ace0c14a07
Dragos <dragosedelea@mai.ru>
2022-03-17 15:32:46 +0200

    Added JavaScript improvements.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-function show() {
-    var formular = document.getElementById("form");
+function show1() {
+    let formular = document.getElementById("form1");
     if (formular.style.display === "") {
         formular.style.display = "block";
     } else if (formular.style.display === "none") {
@@ -8,4 +8,97 @@ function show() {
         formular.style.display = "none";
 
     }
+}
+function togglePopup1() {
+    let formular = document.getElementById("form1");
+    if (formular.style.display === "block") {
+    } formular.style.display = "none";
+}
+
+var form = document.getElementById("formular");
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("formular")),
+    }).then(() =>{
+        alert("Multumesc !");
+        form.reset();
+    });
+});
+
+
+
+function show2() {
+    let formular = document.getElementById("form2");
+    if (formular.style.display === "") {
+        formular.style.display = "block";
+    } else if (formular.style.display === "none") {
+        formular.style.display = "block";
+    } else {
+        formular.style.display = "none";
+
+    }
+}
+function togglePopup2() {
+    let formular = document.getElementById("form2");
+    if (formular.style.display === "block") {
+    } formular.style.display = "none";
+}
+
+
+
+function show3() {
+    let formular = document.getElementById("form3");
+    if (formular.style.display === "") {
+        formular.style.display = "block";
+    } else if (formular.style.display === "none") {
+        formular.style.display = "block";
+    } else {
+        formular.style.display = "none";
+
+    }
+}
+function togglePopup3() {
+    let formular = document.getElementById("form3");
+    if (formular.style.display === "block") {
+    } formular.style.display = "none";
+}
+
+
+
+function show4() {
+    let formular = document.getElementById("form4");
+    if (formular.style.display === "") {
+        formular.style.display = "block";
+    } else if (formular.style.display === "none") {
+        formular.style.display = "block";
+    } else {
+        formular.style.display = "none";
+
+    }
+}
+function togglePopup4() {
+    let formular = document.getElementById("form4");
+    if (formular.style.display === "block") {
+    } formular.style.display = "none";
+}
+
+
+
+function show5() {
+    let formular = document.getElementById("form5");
+    if (formular.style.display === "") {
+        formular.style.display = "block";
+    } else if (formular.style.display === "none") {
+        formular.style.display = "block";
+    } else {
+        formular.style.display = "none";
+
+    }
+}
+function togglePopup5() {
+    let formular = document.getElementById("form5");
+    if (formular.style.display === "block") {
+    } formular.style.display = "none";
 }
